543fd7a5f7506a3878adfcac4b1c16a2d152e100
Dibyanshu Chaubey <134802210+DibyanshuChaubey@users.noreply.github.com>
2025-12-13 06:07:19 +0530

    Add new allowed origin for CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://mynoteapp-nine.vercel.app",
+        "https://frontend-assignment-dibyanshu-chaubey.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
